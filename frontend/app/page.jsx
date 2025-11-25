bcbde658d3d79f5e5b642fbbd49e87b43cb4c347
Shawn Lin <shawnlin@hkpc.org>
2025-11-26 02:46:21 +0800

    change page header

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -111,7 +111,7 @@ export default function Home() {
         {/* Header */}
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-bold tracking-wider">
-            <span className="text-lime-400">HKDC/SGDC</span> FX Desk
+            <span className="text-lime-400">SCCS</span> - StableCoin CyberSpace
           </h1>
 
           <div className="flex items-center gap-3">
